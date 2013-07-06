90f6d7d0e4b54f30a9e11416c73855248bbd9f0b
Jin Jin <bmouse1006@msn.com>
2013-07-06 11:19:55 +0800

    Initial Commit

---
+++ smileshot/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  smileshot
+//
+//  Created by Jin Jin on 13-7-6.
+//  Copyright (c) 2013年 Jin Jin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ smileshotTests/smileshotTests.m
@@ -0,0 +1,36 @@
+//
+//  smileshotTests.m
+//  smileshotTests
+//
+//  Created by Jin Jin on 13-7-6.
+//  Copyright (c) 2013年 Jin Jin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface smileshotTests : XCTestCase
+
+@end
+
+@implementation smileshotTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
